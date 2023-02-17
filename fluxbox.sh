943eadf725d2f236b1d90b5037e0986db4671947
valetzx <45054400+valetzx@users.noreply.github.com>
2023-02-17 10:35:37 +0000

    bypass replit

--- fluxbox.sh
+++ fluxbox.sh
@@ -3,17 +3,7 @@ if [ ! -d "~/nginx" ];then
 	\cp -ax .nginx ~/nginx
 fi
 
-if [ ! -d "~/v2ray" ];then
-	\cp -ax .v2ray ~/v2ray
-fi
-
 sleep 10
-
-chmod u+x ~/v2ray/sbin/v2ray
 chmod u+x ~/nginx/sbin/nginx
-
-~/v2ray/sbin/v2ray >/dev/null 2>&1 & echo done v2ray
 ~/nginx/sbin/nginx -g 'daemon off;' &  echo done nginx
-
 # watch -n 0 "fluxbox amd64" 
-./alist-3.2.0 server
